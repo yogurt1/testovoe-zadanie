dfc7c3e86610eae1aeb7a5d163a55e3ccf5b7a6c
yogurt1 <stalinswag1@gmail.com>
2016-07-28 21:27:05 +0300

    sql_requests: final changes, must work

--- sql_requests.sql
+++ sql_requests.sql
@@ -1,30 +1,28 @@
 /* 1 */
 select clients.name
 from orders
-  inner join clients on clients.id=orders.client_id
+  inner join clients on clients.id = orders.client_id
 where orders.order_date >= date_sub(curdate(), interval 7 day); # last 7 days
 
 /* 2 */
 select clients.name
 from orders
   inner join clients on clients.id=orders.client_id
-where ; 
+order by orders.id
+limit 5;
 
 /* 3 */
 select clients.name
 from orders
-  inner join clients on clients.id=orders.client_id
-where ;
+  inner join clients on clients.id = orders.client_id
+  inner join merchandise on merchandise.id = orders.item_id
+group by merchandise.cost
+order by count(merchandise.cost)
+limit 10;
 
 /* 4 */
 select clients.name
 from orders
-  inner join clients on clients.id=orders.client_id
-where ;
-
-/* 5 */
-select clients.name
-from orders
-  inner join merchendaise on merchendaise.id=orders.item_id
-  inner join clients on clients.id=orders.client_id
+  inner join merchandise on merchandise.id = orders.item_id
+  inner join clients on clients.id = orders.client_id
 where status <> 'complete';
