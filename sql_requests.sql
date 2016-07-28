47f1316fc10892131c0b30ab73dd995e19b5aba4
yogurt1 <stalinswag1@gmail.com>
2016-07-28 17:24:18 +0300

    SQL: interval 7 day

--- sql_requests.sql
+++ sql_requests.sql
@@ -1,7 +1,30 @@
+/* 1 */
+select clients.name
+from orders
+  inner join clients on clients.id=orders.client_id
+where orders.order_date >= date_sub(curdate(), interval 7 day); # last 7 days
 
+/* 2 */
+select clients.name
+from orders
+  inner join clients on clients.id=orders.client_id
+where ; 
+
+/* 3 */
+select clients.name
+from orders
+  inner join clients on clients.id=orders.client_id
+where ;
+
+/* 4 */
+select clients.name
+from orders
+  inner join clients on clients.id=orders.client_id
+where ;
 
 /* 5 */
-select merchandise.id
-from merchandise
-  inner join orders on merchandise.id=orders.item_id
+select clients.name
+from orders
+  inner join merchendaise on merchendaise.id=orders.item_id
+  inner join clients on clients.id=orders.client_id
 where status <> 'complete';
